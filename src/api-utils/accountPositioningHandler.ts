04fea3dba9cfb15f5ba84b2e9543f0d85c2c596f
Code Duchess <princessuhiene@gmail.com>
2024-07-26 22:23:35 +0100

    Update accountPositioningHandler.ts

--- src/api-utils/accountPositioningHandler.ts
+++ src/api-utils/accountPositioningHandler.ts
@@ -6,18 +6,42 @@ export const accountPositioningHandler = async (
   copiedTrader: Trader,
   trader: Trader
 ) => {
+  const [traderInfo, copiedTraderInfo] = [
+    await tradeInfoHandler(trader),
+    await tradeInfoHandler(copiedTrader),
+  ];
 
+  const positioningRatio =
+    Math.floor(
+      (copiedTraderInfo.portfolioValue / traderInfo.portfolioValue) * 1000
+    ) / 1000;
+
+  AppState.set("positioningRatio", positioningRatio);
+
+  console.log(`Positioning Ratio: ${positioningRatio}`);
+
+  return await copyInitialAccountHandler(
+    trader,
+    traderInfo.positions,
+    copiedTraderInfo.positions,
+    positioningRatio
+  );
 };
 
 const tradeInfoHandler = async (trader: Trader) => {
+  await trader.update();
 
+  const positions = trader.getPositions();
+  const portfolioValue = trader.getPortfolioValue().toDecimal();
+
+  return { positions, portfolioValue };
 };
 
 const copyInitialAccountHandler = async (
-  trader: Trader,
-  traderPositions: Map<any, any>,
-  copiedTraderPositions: Map<any, any>,
-  positioningRatio: number
+trader: Trader,
+traderPositions: Map<any, any>,
+copiedTraderPositions: Map<any, any>,
+positioningRatio: number
 ) => {
 
 };
