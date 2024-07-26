3a87460b4f667ff52d5c32f580b81545c7680c54
Code Duchess <princessuhiene@gmail.com>
2024-07-26 22:19:08 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,29 @@ import { tradeHandler } from "./api-utils/tradeHandler";
 
 const AppState = new Map<string, any>();
 
-export const app = async () => {
+const server = Bun.serve({
+  async fetch(req, server) {
+    const url = new URL(req.url);
+    const { pathname, searchParams } = url;
 
-  const server = Bun.serve({
-    async fetch(req, server) {
-      const url = new URL(req.url);
-      const { pathname, searchParams } = url;
+    let response: Response | undefined = new Response(
+      JSON.stringify({ status: 200 })
+    );
 
-      let response: Response | undefined = new Response(
-        JSON.stringify({ status: 200 })
-      );
+    switch (pathname) {
+      case "/process-trade":
+        break;
+      case "/new-subscription":
+        break;
+      case "/cancel-subscription":
+        break;
+      default:
+        break;
+    }
 
-      switch (pathname) {
-        case "/process-trade":
-          break;
-        case "/new-subscription":
-          break;
-        case "/cancel-subscription":
-          break;
-        default:
-          break;
-      }
-
-      if (!response) return new Response(JSON.stringify({ status: 200 }));
-      return response;
-    },
-  });
-  console.log(`${server.url}`);
+    if (!response) return new Response(JSON.stringify({ status: 200 }));
+    return response;
+  },
+});
+console.log(`Running on: ${server.hostname} | Port: ${server.port}`);
 };
